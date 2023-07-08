afa796f292a55ad7dc75562aad7530db476101dd
Raimones Barros <raimonesrg3@gmail.com>
2023-07-08 15:29:16 -0300

    feat: add dark mode to Storybook

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,12 +1,19 @@
 /** @type { import('@storybook/react').Preview } */
+
+import { themes } from '@storybook/theming'
+
 const preview = {
   parameters: {
+    layout: 'centered',
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    docs: {
+      theme: themes.dark
     },
   },
 };
