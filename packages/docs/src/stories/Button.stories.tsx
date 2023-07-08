6451b854ed8a8db138dd0fdb26cf55b0c2e63b93
Raimones Barros <raimonesrg3@gmail.com>
2023-07-08 19:36:55 -0300

    feat: create Button Story

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@raimones-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+  tags: ['autodocs'],
+
+  args: {
+    children: 'Voltar',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'small',
+  },
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,30 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
-  backgroundColor: '$rai500',
-  padding: '$2 $4',
-  color: '$white',
+export const Button = styled('button', {
   fontFamily: '$code',
+  backgroundColor: '$rai300',
+  borderRadius: '$sm',
+  border: 0,
   fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Voltar</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
